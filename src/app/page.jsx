c7baa54840032073406032205a41752816b2070b
duongkien27 <duongkien.hust@gmail.com>
2024-06-12 22:11:12 +0700

    240612 update component Feature, Category and Navbar

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,7 @@
 import Featured from "@/components/featured/Featured";
 import styles from "./homepage.module.css";
 import Link from "next/link"
-import CategoryList from "@/components/categoryList/categoryList";
+import CategoryList from "@/components/categoryList/CategoryList";
 import CardList from "@/components/cardList/CardList";
 import Menu from "@/components/menu/Menu";
 
--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
 import styles from "./featured.module.css"
+import Image from 'next/image'
 const Featured = () => {
   return (
-    <div className={styles.container}>Featured</div>
+    <div className={styles.container}>
+      <h1 className={styles.title}>
+        <b>This is Duong Kien!</b> Welcome to my blog!
+      </h1>
+      <div className={styles.post}>
+        <div className={styles.imgContainer}>
+          <Image src="/p1.jpeg" alt="" fill className={styles.image}></Image>
+        </div>
+        <div className={styles.textContainer}>
+          <h1 className={styles.postTitle}>Post Title</h1>
+          <p className={styles.postDesc}>
+          This picture was taken in a room. 
+          There 2 people in this picture. 
+          On the right, a men with a short hair are lifting a sofa. 
+          On the left, a women is holding a box with a potted plant on top of it.
+          </p>
+          <button className={styles.button}>Read More</button>
+        </div>
+      </div>
+    </div>
   )
 }
 
