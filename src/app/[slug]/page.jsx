e23036503ebff7bd46405152cf42e6b0e31e8351
duongkien27 <duongkien.hust@gmail.com>
2024-12-29 19:19:06 +0700

    20241229 Update single page

--- src/app/[slug]/page.jsx
+++ src/app/[slug]/page.jsx
@@ -1,29 +1,42 @@
 import React from 'react'
 import styles from "./singlePage.module.css"
 import Image from 'next/image'
+import Menu from '@/components/menu/Menu'
 
 const SinglePage = () => {
-  return (
-    <div className={styles.container}>
-        <div className={styles.infoContainer}>
-            <div className={styles.textContainer}></div>
-                <h1>Single Blog Page</h1>
-                <div className={styles.user}>
-                    <div className={styles.userImageContainer}>
-                        <Image src="" alt="" fill className={styles.iamge} />
-                    </div>
-                    <div className={styles.userTextContainer}>
-                        <span className={styles.username}>Duong Kien</span>
-                        <span className={styles.date}>01.01.2024</span>
-                    </div>
+    return (
+        <div className={styles.container}>
+          <div className={styles.infoContainer}>
+            <div className={styles.textContainer}>
+              <h1 className={styles.title}>Single Page</h1>
+              <div className={styles.user}>
+              <div className={styles.userImageContainer}>
+                <Image src="/p1.jpeg" alt="" fill className={styles.avatar} />
                 </div>
-            <div className={styles.imageContainer}>
-                <Image src="" alt="" fill className={styles.iamge} />
-                <Image src="" alt="" fill className={styles.iamge} />
+                <div className={styles.userTextContainer}>
+                  <span className={styles.username}>Duong Kien</span>
+                  <span className={styles.date}>01.01.2024</span>
+                </div>
+              </div>
             </div>
+            <div className={styles.imageContainer}>
+                <Image src="/p1.jpeg" alt="" fill className={styles.image} />
+            </div>
+          </div>
+          <div className={styles.content}>
+            <div className={styles.post}>
+              <div
+                className={styles.description}
+                
+              />
+              <div className={styles.comment}>
+                
+              </div>
+            </div>
+            <Menu />
+          </div>
         </div>
-    </div>
-  )
+      );
 }
 
 export default SinglePage
